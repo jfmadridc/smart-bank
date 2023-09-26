6e1d2863be1dd0633ed5253c3cf64ee7272fa199
JuanF Madrid <jfm@jfmadrid.com>
2023-09-26 15:49:41 -0500

    Created a global style component
    Created a global style component using the "styled-components" library, that sets the box-sizing, font-family, margin, padding, text-decoration, and color properties for all elements to achieve a consistent visual style throughout the application.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from "react";
 
 import Container from "./Components/Container";
 import Header from "./Components/Header";
+import GlobalStyle from "./GlobalStyle";
 
 function App() {
-  return (
-    <>
-      <Header />
-      <Container />
-    </>
-  );
+	return (
+		<>
+			<GlobalStyle />
+			<Header />
+			<Container />
+		</>
+	);
 }
 
 export default App;

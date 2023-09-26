f95f8819088e3622441018370d711e83d8ef6c02
JuanF Madrid <jfm@jfmadrid.com>
2023-09-25 23:11:16 -0500

    Starting the project, changing some labels to English

--- src/Components/Account/index.jsx
+++ src/Components/Account/index.jsx
@@ -5,37 +5,37 @@ import ojo from "../../assets/images/ojo.svg";
 import dinero from "../../assets/images/dinero.svg";
 
 const Account = () => {
-  const [toggleState, untoggle] = useState(true);
+	const [toggleState, untoggle] = useState(true);
 
-  const toggleHandler = () => {
-    untoggle((toggleState) => !toggleState);
-  };
+	const toggleHandler = () => {
+		untoggle((toggleState) => !toggleState);
+	};
 
-  return (
-    <div className="box">
-      <h2>Cuenta</h2>
-      <div style={{ fontSize: "26px", padding: "20px 0" }}>
-        Saldo disponible
-        <span>
-          <img className="imagen-icono" src={dinero} alt="Icono de saldo" />
-        </span>
-        {toggleState ? (
-          <div className="saldo">
-            <span className="detalle">$</span> 8,621.50
-          </div>
-        ) : null}
-      </div>
+	return (
+		<div className="box">
+			<h2>Account</h2>
+			<div style={{ fontSize: "26px", padding: "20px 0" }}>
+				Balance Available
+				<span>
+					<img className="imagen-icono" src={dinero} alt="Icono de saldo" />
+				</span>
+				{toggleState ? (
+					<div className="saldo">
+						<span className="detalle">$</span> 8,621.50
+					</div>
+				) : null}
+			</div>
 
-      <button className="btn" onClick={toggleHandler}>
-        <img
-          style={{ marginTop: "2px" }}
-          className="imagen-icono"
-          src={toggleState ? privado : ojo}
-          alt="Privacidad de saldo"
-        />
-      </button>
-    </div>
-  );
+			<button className="btn" onClick={toggleHandler}>
+				<img
+					style={{ marginTop: "2px" }}
+					className="imagen-icono"
+					src={toggleState ? privado : ojo}
+					alt="Privacidad de saldo"
+				/>
+			</button>
+		</div>
+	);
 };
 
 export default Account;

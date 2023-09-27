b7a41b21912dcd8a42735c39aca04f5ca869af2c
JuanF Madrid <jfm@jfmadrid.com>
2023-09-27 10:56:37 -0500

    Starting to implement dark theme

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import React from "react";
 import Container from "./Components/Container";
 import Header from "./Components/Header";
 import GlobalStyle from "./GlobalStyle";
+import { temaClaro, temaOscuro } from "./Components/UI/temas";
+import { ThemeProvider } from "styled-components";
 
 function App() {
 	return (
-		<>
+		<ThemeProvider theme={temaOscuro}>
 			<GlobalStyle />
 			<Header />
 			<Container />
-		</>
+		</ThemeProvider>
 	);
 }
 
--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import logo from "../../assets/images/logo.svg";
-import { colorPrimario } from "../UI/Variables";
+import { colorPrimario } from "../UI/variables";
 
 const StyledHeader = styled.header`
 	background-color: ${colorPrimario};

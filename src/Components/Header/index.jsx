85839ed6f2ccdbafdfc71bb8cf9b142786d6d4ae
JuanF Madrid <jfm@jfmadrid.com>
2023-09-26 19:12:23 -0500

    Refactored to used Variables for colors

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import styled from "styled-components";
 import logo from "../../assets/images/logo.svg";
+import { colorPrimario } from "../UI/Variables";
 
 const StyledHeader = styled.header`
-	background-color: #41d3be;
+	background-color: ${colorPrimario};
 	display: flex;
 	justify-content: space-between;
 	padding: 0 15vw;
@@ -11,10 +12,15 @@ const StyledHeader = styled.header`
 	align-items: center;
 `;
 
+const Logo = styled.img`
+	height: 50px;
+	width: 50px;
+`;
+
 const Header = () => {
 	return (
 		<StyledHeader>
-			<img className="imagen-logo" src={logo} alt="Logo Smart Bank" />
+			<Logo src={logo} alt="Logo Smart Bank" />
 			<div>
 				<a className="btn-secundario" href="https://google.com">
 					Help

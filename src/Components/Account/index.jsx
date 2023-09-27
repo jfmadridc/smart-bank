9af9c5102ceb45659ca71f092a8828a211636aee
JuanF Madrid <jfm@jfmadrid.com>
2023-09-26 21:45:45 -0500

    Converting Inline CSS to a inheritance Styled Comp

--- src/Components/Account/index.jsx
+++ src/Components/Account/index.jsx
@@ -1,9 +1,14 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 import { Icono } from "../UI";
 
 import privado from "../../assets/images/privado.svg";
 import ojo from "../../assets/images/ojo.svg";
 import dinero from "../../assets/images/dinero.svg";
+
+const IconoMargin = styled(Icono)`
+	margin-top: 2px;
+`;
 
 const Account = () => {
 	const [toggleState, untoggle] = useState(true);
@@ -28,11 +33,7 @@ const Account = () => {
 			</div>
 
 			<button className="btn" onClick={toggleHandler}>
-				<Icono
-					style={{ marginTop: "2px" }}
-					src={toggleState ? privado : ojo}
-					alt="Privacidad de saldo"
-				/>
+				<IconoMargin src={toggleState ? privado : ojo} alt="Privacidad de saldo" />
 			</button>
 		</div>
 	);
